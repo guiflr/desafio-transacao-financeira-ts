7ccc9e8c22377a2be2bb93475f404d71ab31a100
guiflr <guiflr97@gmail.com>
2021-06-24 14:58:46 -0300

    Server para 5556

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import app from './app';
 
-app.listen(3344, () => {
-  console.log('🚀 Server started on port 3344!');
+app.listen(5555, () => {
+  console.log('🚀 Server started on port 5555!');
 });
